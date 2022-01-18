431827a1030cb57fdee639467f5a48d0913753e4
RafaelRufino <rafaelrufino.52@gmail.com>
2022-01-18 12:56:37 -0300

    criando caso de uso de especificações de carros

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,22 @@
+import { ISpecificationsRespository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationRepository: ISpecificationsRespository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification  already exists!");
+    }
+
+    this.specificationRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationUseCase };
